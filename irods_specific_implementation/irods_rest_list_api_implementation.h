b03d6c9e5671e85055b0748abfdd5c5778e993c8
Jason Coposky <jasonc@renci.org>
2019-10-08 17:12:05 -0400

    Work for Collection Iteration

---
+++ irods_specific_implementation/irods_rest_list_api_implementation.h
@@ -0,0 +1,196 @@
+
+#include "irods_rest_api_base.h"
+
+#include "filesystem.hpp"
+
+// this is contractually tied directly to the swagger api definition, and the below implementation
+#define MACRO_IRODS_LIST_API_IMPLEMENTATION \
+    Pistache::Http::Code code; \
+    std::string message; \
+    std::tie(code, message) = irods_list_(headers.getRaw("Authorization").value(), path.get(), stat.get(), permissions.get(), metadata.get(), offset.get(), limit.get()); \
+    response.send(code, message);
+
+namespace irods::rest {
+
+namespace fs   = irods::experimental::filesystem;
+namespace fcli = irods::experimental::filesystem::client;
+using     fsp  = fs::path;
+
+class list : api_base {
+    public:
+    std::tuple<Pistache::Http::Code &&, std::string> operator()(
+        const std::string& _auth_header,
+        const std::string& _logical_path,
+        const std::string& _stat,
+        const std::string& _permissions,
+        const std::string& _metadata,
+        const std::string& _offset,
+        const std::string& _limit) {
+
+        auto conn = get_connection();
+        authenticate(conn(), _auth_header);
+
+        try {
+
+            std::string logical_path{decode_url(_logical_path)};
+
+            uintmax_t limit_counter{0};
+            uintmax_t offset_counter{0};
+            const uintmax_t offset = std::stoi(_offset);
+            const uintmax_t limit  = std::stoi(_limit);
+
+            const bool stat        = ("true" == _stat);
+            const bool permissions = ("true" == _permissions);
+            const bool metadata    = ("true" == _metadata);
+
+            nlohmann::json objects = nlohmann::json::array();
+
+            fsp start_path{_logical_path};
+            for(auto p : fcli::recursive_collection_iterator(*conn(), start_path)) {
+                // skip earlier entries for paging
+                if(offset > 0 && offset_counter < offset) {
+                    ++offset_counter;
+                    continue;
+                }
+
+                try {
+                    auto object_status = fcli::status(*conn(), p.path());
+
+                    nlohmann::json obj_info = nlohmann::json::object();
+                    obj_info["type"] = type_to_string[object_status.type()];
+                    obj_info["logical_path"] = p.path().string();
+
+
+                    if(stat) { aggregate_stat_information(*conn(), obj_info, p.path()); }
+                    if(permissions) { aggregate_permissions_information(obj_info, object_status.permissions()); }
+                    if(metadata) { aggregate_metadata_information(*conn(), obj_info, p.path()); }
+
+                    objects += obj_info;
+                }
+                catch(const exception& _e) {
+                    rodsLog(
+                        LOG_ERROR,
+                        "failed to capture results for object [%s]",
+                        p.path().string().c_str());
+                }
+
+                ++limit_counter;
+                if(limit > 0 && limit_counter >= limit) {
+                    break;
+                }
+            } // for path
+
+            nlohmann::json results = nlohmann::json::object();
+            results["_embedded"] = objects;
+
+            nlohmann::json links = nlohmann::json::object();
+            std::string base_url{"list?path=%s&stat=%s&permissions=%smetadata=%s?offset=%s?limit=%s"};
+            links["self"] = boost::str(boost::format(base_url)
+                            % _logical_path
+                            % _stat
+                            % _permissions
+                            % _metadata
+                            % _offset
+                            % _limit);
+            links["first"] = boost::str(boost::format(base_url)
+                            % _logical_path
+                            % _stat
+                            % _permissions
+                            % _metadata
+                            % "0"
+                            % _limit);
+            links["last"] = boost::str(boost::format(base_url)
+                            % _logical_path
+                            % _stat
+                            % _permissions
+                            % _metadata
+                            % "FIXME"
+                            % _limit);
+            links["next"] = boost::str(boost::format(base_url)
+                            % _logical_path
+                            % _stat
+                            % _permissions
+                            % _metadata
+                            % std::to_string(offset+limit)
+                            % _limit);
+            links["prev"] = boost::str(boost::format(base_url)
+                            % _logical_path
+                            % _stat
+                            % _permissions
+                            % _metadata
+                            % std::to_string(std::max((uintmax_t)0, offset-limit))
+                            % _limit);
+            results["_links"] = links;
+
+            return std::forward_as_tuple(
+                    Pistache::Http::Code::Ok,
+                    results.dump());
+        }
+        catch(const irods::exception& _e) {
+            return std::forward_as_tuple(
+                    Pistache::Http::Code::Bad_Request,
+                    _e.what());
+        }
+    } // operator()
+
+    private:
+    std::map<irods::experimental::filesystem::perms, std::string> perm_to_string = {
+        {irods::experimental::filesystem::perms::null,      "null"},
+        {irods::experimental::filesystem::perms::read,      "read"},
+        {irods::experimental::filesystem::perms::write,     "write"},
+        {irods::experimental::filesystem::perms::own,       "own"},
+        {irods::experimental::filesystem::perms::inherit,   "inherit"},
+        {irods::experimental::filesystem::perms::noinherit, "noinherit"},
+     };
+
+    std::map<irods::experimental::filesystem::object_type, std::string> type_to_string = {
+        {irods::experimental::filesystem::object_type::none,               "none"},
+        {irods::experimental::filesystem::object_type::not_found,          "not_found"},
+        {irods::experimental::filesystem::object_type::data_object,        "data_object"},
+        {irods::experimental::filesystem::object_type::collection,         "collection"},
+        {irods::experimental::filesystem::object_type::special_collection, "special_collection"},
+        {irods::experimental::filesystem::object_type::unknown,            "unknown"},
+    };
+
+    void aggregate_stat_information(
+        rcComm_t&       _comm,
+        nlohmann::json& _obj_info,
+        const fs::path& _path) {
+
+        nlohmann::json stat_info = nlohmann::json::object();
+        stat_info["size"] = fcli::data_object_size(_comm, _path);
+        auto last_write_time{std::chrono::system_clock::to_time_t(fcli::last_write_time(_comm, _path))};
+        stat_info["last_write_time"] = std::to_string(last_write_time);
+
+        _obj_info["status_information"] = stat_info;
+    } // aggregate_stat_information
+
+    void aggregate_permissions_information(
+        nlohmann::json&                           _obj_info,
+        const std::vector<fs::entity_permission>& _perms) {
+        nlohmann::json perm_info = nlohmann::json::object();
+
+        for(auto p : _perms) {
+            perm_info[p.name] = perm_to_string[p.prms];
+        }
+
+        _obj_info["permission_information"] = perm_info;
+    } // aggregate_permissions_information
+
+    void aggregate_metadata_information(
+        rcComm_t&       _comm,
+        nlohmann::json& _obj_info,
+        const fs::path& _path) {
+        nlohmann::json meta_arr = nlohmann::json::array();
+        for(auto avu : fcli::get_metadata(_comm, _path)) {
+            nlohmann::json md = nlohmann::json::object();
+            md["attribute"] = avu.attribute;
+            md["value"] = avu.value;
+            md["unit"] = avu.units;
+            meta_arr += md;
+        }
+
+        _obj_info["metadata"] = meta_arr;
+    } // aggregate_metadata_information
+}; // class list
+}; // namespace irods::rest
