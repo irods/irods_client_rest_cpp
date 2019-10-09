a45b48df61f93fc1d13920df96df080f14a99159
Jason Coposky <jasonc@renci.org>
2019-10-08 21:53:06 -0400

    Add math for proper paging _links urls with query interface

--- irods_specific_implementation/irods_rest_query_api_implementation.h
+++ irods_specific_implementation/irods_rest_query_api_implementation.h
@@ -10,8 +10,7 @@
     std::tie(code, message) = irods_query_(headers.getRaw("Authorization").value(), queryString.get(), queryLimit.get(), rowOffset.get(), queryType.get()); \
     response.send(code, message);
 
-namespace irods {
-namespace rest {
+namespace irods::rest {
 class query : api_base {
     public:
     std::tuple<Pistache::Http::Code &&, std::string> operator()(
@@ -33,8 +32,11 @@ class query : api_base {
             auto query_type = irods::query<rcComm_t>::convert_string_to_query_type(_query_type);
             irods::query<rcComm_t> qobj{conn(), query_string, query_limit, row_offset, query_type};
 
+            uintmax_t current_row_count{0};
+            uintmax_t total_row_count{qobj.size()};
             nlohmann::json arrays = nlohmann::json::array();
             for(auto row : qobj) {
+                ++current_row_count;
                 nlohmann::json array = nlohmann::json::array();
                 for(auto e : row) {
                     array += e;
@@ -43,9 +45,16 @@ class query : api_base {
                 arrays += array;
             }
 
+            total_row_count += row_offset;
+
             nlohmann::json results = nlohmann::json::object();
             results["_embedded"] = arrays;
+            results["count"] = std::to_string(current_row_count);
+            results["total"] = std::to_string(total_row_count);
 
+            double dbl_row_offset  = static_cast<double>(row_offset);
+            double dbl_query_limit = static_cast<double>(query_limit);
+            double dbl_total_row_count = static_cast<double>(total_row_count);
             nlohmann::json links = nlohmann::json::object();
             std::string base_url{"query?query_string=%s&query_limit=%s&row_offset=%s&query_type=%s"};
             links["self"] = boost::str(boost::format(base_url)
@@ -53,25 +62,40 @@ class query : api_base {
                             % _query_limit
                             % _row_offset
                             % _query_type);
+
             links["first"] = boost::str(boost::format(base_url)
                             % _query_string
                             % _query_limit
                             % "0"
                             % _query_type);
+
+            double total_pages = dbl_total_row_count / dbl_query_limit;
+            double fraction_remaining_pages = (total_pages - std::trunc(total_pages));
+            double remaining_rows = fraction_remaining_pages * static_cast<double>(dbl_query_limit);
+            double final_page_delta{remaining_rows == 0.0 ? dbl_query_limit : remaining_rows};
+            double last_page_number{dbl_total_row_count - final_page_delta};
+
             links["last"] = boost::str(boost::format(base_url)
                             % _query_string
                             % _query_limit
-                            % "FIXME"
+                            % std::to_string(static_cast<int>(last_page_number))
                             % _query_type);
+
+            double current_page_number = dbl_row_offset / dbl_query_limit;
+            double next_page_number{std::trunc(current_page_number)+1 * dbl_query_limit};
+            next_page_number = next_page_number >= dbl_total_row_count ? last_page_number : next_page_number;
+
             links["next"] = boost::str(boost::format(base_url)
                             % _query_string
                             % _query_limit
-                            % std::to_string(row_offset+query_limit)
+                            % std::to_string(static_cast<int>(next_page_number))
                             % _query_type);
+
+            auto prev_count = dbl_row_offset - dbl_query_limit;
             links["prev"] = boost::str(boost::format(base_url)
                             % _query_string
                             % _query_limit
-                            % std::to_string(std::max((uintmax_t)0, row_offset-query_limit))
+                            % std::to_string(static_cast<int>(std::max(0.0, prev_count)))
                             % _query_type);
             results["_links"] = links;
 
@@ -86,5 +110,4 @@ class query : api_base {
         }
     } // operator()
 }; // class query
-}; // namespace rest
-}; // namespace irods
+}; // namespace irods::rest
