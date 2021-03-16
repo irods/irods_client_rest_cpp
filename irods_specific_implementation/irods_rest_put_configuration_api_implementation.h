9967f34cea8e48620473ce0eeb9339ec8de9484d
Jason Coposky <jasonc@renci.org>
2021-03-16 15:03:57 -0400

    [#7] do not forward temporary as tuple

--- irods_specific_implementation/irods_rest_put_configuration_api_implementation.h
+++ irods_specific_implementation/irods_rest_put_configuration_api_implementation.h
@@ -9,9 +9,7 @@
 
 // this is contractually tied directly to the pistache implementation, and the below implementation
 #define MACRO_IRODS_CONFIGURATION_PUT_API_IMPLEMENTATION \
-    Pistache::Http::Code code; \
-    std::string message; \
-    std::tie(code, message) = irods_put_configuration_(request.headers().getRaw("Authorization").value(), cfg.get()); \
+    auto [code, message] = irods_put_configuration_(request.headers().getRaw("Authorization").value(), cfg.get()); \
     response.send(code, message);
 
 namespace irods::rest {
@@ -41,16 +39,6 @@ namespace irods::rest {
 
         } // throw_if_user_is_not_rodsadmin
 
-        auto ends_with_dot_json(const bfs::directory_entry& _e) -> bool
-        {
-            const auto s = std::string{".json"};
-            const auto l = _e.path().string().length();
-            const auto p = _e.path().string();
-            const auto x = p.substr(l-s.length());
-
-            return  x == s;
-        }
-
         public:
 
         put_configuration() : api_base{service_name}
@@ -62,9 +50,9 @@ namespace irods::rest {
               const std::string& _auth_header
             , const std::string& _configuration) -> std::tuple<Pistache::Http::Code &&, std::string>
         {
-
             try {
                 auto conn = get_connection(_auth_header);
+
                 throw_if_user_is_not_rodsadmin(conn);
 
                 auto dir = get_irods_config_directory();
@@ -85,17 +73,13 @@ namespace irods::rest {
                     }
 
                     os << v.at("contents").dump(4).c_str();
+
                 } // for key, val
 
-                if(err.empty()) {
-                    return std::forward_as_tuple(
-                               Pistache::Http::Code::Ok, "");
-                }
-                else {
-                    return std::forward_as_tuple(
-                               Pistache::Http::Code::Bad_Request,
-                               err);
-                }
+                return std::forward_as_tuple(
+                            err.empty()
+                            ? Pistache::Http::Code::Ok
+                            : Pistache::Http::Code::Bad_Request, err);
             }
             catch(const irods::exception& _e) {
                 auto error = make_error(_e.code(), _e.client_display_what());
