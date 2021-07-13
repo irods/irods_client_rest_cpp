348c025d1c1e5b3ac292c116f6e64b6f428473d9
Kory Draughn <korydraughn@renci.org>
2021-07-13 12:00:59 -0400

    [#48] /admin endpoint now decodes resource name, vault path, and zone name.

--- irods_specific_implementation/irods_rest_admin_api_implementation.h
+++ irods_specific_implementation/irods_rest_admin_api_implementation.h
@@ -1,12 +1,14 @@
 #include "irods_rest_api_base.h"
 
 #include "generalAdmin.h"
+#include "rodsErrorTable.h"
 
 // this is contractually tied directly to the swagger api definition, and the below implementation
 #define MACRO_IRODS_ADMIN_API_IMPLEMENTATION \
     Pistache::Http::Code code; \
-    std::string message; \
-    std::tie(code, message) = irods_admin_(headers.getRaw("authorization").value(), action.get(), target.get(), arg2.get(), arg3.get(), arg4.get(), arg5.get(), arg6.get(), arg7.get()); \
+    std::string message;  \
+    std::tie(code, message) = irods_admin_(headers.getRaw("authorization").value(), action.get(), \
+        target.get(), arg2.get(), arg3.get(), arg4.get(), arg5.get(), arg6.get(), arg7.get()); \
     response.send(code, message);
 
 namespace irods::rest
@@ -17,48 +19,62 @@ namespace irods::rest
     class admin : public api_base
     {
     public:
-        admin() : api_base{service_name}
+        admin()
+            : api_base{service_name}
         {
             logger_->trace("Endpoint [{}] initialized.", service_name);
         }
 
-        std::tuple<Pistache::Http::Code&&, std::string> operator()(
-            const std::string& _auth_header,
-            const std::string& _action,
-            const std::string& _target,
-            const std::string& _arg2,
-            const std::string& _arg3,
-            const std::string& _arg4,
-            const std::string& _arg5,
-            const std::string& _arg6,
-            const std::string& _arg7)
+        std::tuple<Pistache::Http::Code, std::string>
+        operator()(const std::string& _auth_header,
+                   const std::string& _action,
+                   const std::string& _target,
+                   const std::string& _arg2,
+                   const std::string& _arg3,
+                   const std::string& _arg4,
+                   const std::string& _arg5,
+                   const std::string& _arg6,
+                   const std::string& _arg7)
         {
+            logger_->trace("Handling /admin request ...");
+
+            logger_->debug("_auth_header=[{}], _action=[{}], _target=[{}], "
+                           "_arg2=[{}], _arg3=[{}], _arg4=[{}], _arg5=[{}], _arg6=[{}]",
+                           _auth_header, _action, _target, _arg2, _arg3, _arg4, _arg5, _arg6);
+
             auto conn = get_connection(_auth_header);
 
             try {
-                auto gen_inp = generalAdminInp_t{
-                                   _action == "remove" ? "rm" : _action.c_str(),
-                                   _target.c_str(),
-                                   _arg2.c_str(),
-                                   _arg3.c_str(),
-                                   _arg4.c_str(),
-                                   _arg5.c_str(),
-                                   _arg6.c_str(),
-                                   _arg7.c_str(),
-                                   nullptr,
-                                   nullptr};
-                auto err = rcGeneralAdmin(conn(), &gen_inp);
-                if(err < 0) {
-                    auto error = make_error(err, "rsGeneralAdmin failed.");
-                    return std::forward_as_tuple(Pistache::Http::Code::Bad_Request, error);
+                const auto resc_name = decode_url(_arg2);
+                const auto vault_path = decode_url(_arg4);
+                const auto zone_name = decode_url(_arg6);
+
+                logger_->debug("decoded arguments - _arg2=[{}], _arg4=[{}], _arg6=[{}]",
+                               resc_name, vault_path, zone_name);
+
+                generalAdminInp_t input{};
+                input.arg0 = (_action == "remove") ? "rm" : _action.c_str();
+                input.arg1 = _target.c_str();
+                input.arg2 = resc_name.c_str();
+                input.arg3 = _arg3.c_str();
+                input.arg4 = vault_path.c_str();
+                input.arg5 = _arg5.c_str();
+                input.arg6 = zone_name.c_str();
+                input.arg7 = _arg7.c_str();
+
+                if (const auto ec = rcGeneralAdmin(conn(), &input); ec < 0) {
+                    return make_error_response(ec, "Error on rcGeneralAdmin.");
                 }
 
-                return std::forward_as_tuple(Pistache::Http::Code::Ok, SUCCESS);
+                return std::make_tuple(Pistache::Http::Code::Ok, SUCCESS);
             }
-            catch(const irods::exception& _e) {
-                auto error = make_error(_e.code(), _e.what());
-                return std::forward_as_tuple(Pistache::Http::Code::Bad_Request, error);
+            catch (const irods::exception& e) {
+                return make_error_response(e.code(), e.what());
+            }
+            catch (const std::exception& e) {
+                return make_error_response(SYS_INVALID_INPUT_PARAM, e.what());
             }
         } // operator()
     }; // class admin
 } // namespace irods::rest
+
