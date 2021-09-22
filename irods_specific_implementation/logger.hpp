249acdb5ea96dbe8def9b87d4bc2cb428966e01f
Kory Draughn <korydraughn@renci.org>
2021-09-22 14:15:32 -0400

    [#57] Changed log message format.

--- irods_specific_implementation/logger.hpp
+++ irods_specific_implementation/logger.hpp
@@ -17,17 +17,15 @@ namespace irods::rest
     {
         constexpr auto enable_formatting = true;
 
-        auto primary_logger = spdlog::syslog_logger_mt(_service_name.data(), "", LOG_PID, LOG_LOCAL0, enable_formatting);
-        auto json_logger = spdlog::syslog_logger_mt("json", "", LOG_PID, LOG_LOCAL0, enable_formatting);
+        auto logger = spdlog::syslog_logger_mt(_service_name.data(), "", LOG_PID, LOG_LOCAL0, enable_formatting);
 
         try {
-            primary_logger->set_pattern(
-                fmt::format(R"_({{"timestamp": "%Y-%m-%dT%T.%e%z", "service": "{}", )_"
-                            R"_("pid": %P, "thread": %t, "severity": "%l", "message": "%v"}})_", _service_name));
-
-            json_logger->set_pattern(
-                fmt::format(R"_({{"timestamp": "%Y-%m-%dT%T.%e%z", "service": "{}", )_"
-                            R"_("pid": %P, "thread": %t, "severity": "%l", "message": %v}})_", _service_name));
+            // The log message format does not include whitespace because that is how
+            // the nlohmann JSON library outputs JSON without indentation. This makes
+            // the output appear as if it was generated in realtime rather than being
+            // formatted to appear as JSON.
+            logger->set_pattern(R"_({"timestamp":"%Y-%m-%dT%T.%e%z","service":"%n",)_"
+                                R"_("pid":%P,"thread":%t,"severity":"%l","payload":%v})_");
 
             if (_config.contains(configuration_keywords::log_level)) {
                 namespace keywords = configuration_keywords;
@@ -44,24 +42,21 @@ namespace irods::rest
                 else if (lvl == "trace")    { lvl_enum = spdlog::level::trace; }
                 // clang-format on
 
-                primary_logger->set_level(lvl_enum);
-                json_logger->set_level(lvl_enum);
+                logger->set_level(lvl_enum);
             }
             else {
-                primary_logger->set_level(spdlog::level::info);
-                json_logger->set_level(spdlog::level::info);
+                logger->set_level(spdlog::level::info);
             }
         }
         catch (...) {
-            primary_logger->set_level(spdlog::level::info);
-            json_logger->set_level(spdlog::level::info);
+            logger->set_level(spdlog::level::info);
         }
 
         // This allows us to use the global logger without needing to look it up.
         // This is required so that the API interfaces have a logger.
-        spdlog::set_default_logger(primary_logger);
+        spdlog::set_default_logger(logger);
 
-        return primary_logger;
+        return logger;
     } // init_logger
 } // namespace irods::rest
 
