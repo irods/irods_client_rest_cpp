114157bd93561cc3454a4e0c60ac74d3a35e8ccc
Kory Draughn <korydraughn@renci.org>
2021-10-25 18:46:39 -0400

    [#80] Exposed all ticket options.

--- irods_specific_implementation/irods_rest_access_implementation.h
+++ irods_specific_implementation/irods_rest_access_implementation.h
@@ -6,11 +6,13 @@
 #include "irods_random.hpp"
 #include "rodsErrorTable.h"
 #include "irods_at_scope_exit.hpp"
+#include "irods_query.hpp"
 
 #include "pistache/http_headers.h"
 #include "pistache/optional.h"
 #include "pistache/router.h"
 
+#include "boost/algorithm/string.hpp"
 #include "fmt/format.h"
 #include "json.hpp"
 
@@ -41,26 +43,37 @@ namespace irods::rest
         {
             namespace fs = irods::experimental::filesystem;
 
+            std::string ticket_id;
+
             try {
                 auto _logical_path = _request.query().get("path").get();
+                auto _type = _request.query().get("type");
                 auto _use_count = _request.query().get("use_count");
+                auto _write_file_count = _request.query().get("write_file_count");
+                auto _write_byte_count = _request.query().get("write_byte_count");
                 auto _seconds_until_expiration = _request.query().get("seconds_until_expiration");
+                auto _users = _request.query().get("users");
+                auto _groups = _request.query().get("groups");
+                auto _hosts = _request.query().get("hosts");
 
                 auto conn = get_connection(_request.headers().getRaw("authorization").value());
 
-                // TODO: can we ensure this is a canonical path?
-                const std::string logical_path{decode_url(_logical_path)};
-                debug("Logical path = [{}]", logical_path);
+                const fs::path logical_path = decode_url(_logical_path);
+                debug("Logical path = [{}]", logical_path.c_str());
 
-                const fs::path fs_path = logical_path;
-                const auto size = fs::client::data_object_size(*conn(), fs_path);
-                const auto ticket_id = make_ticket_id();
+                const auto size = fs::client::data_object_size(*conn(), logical_path);
+                ticket_id = make_ticket_id();
 
                 // TODO These calls could benefit from an atomic_apply_ticket_operations API plugin.
                 // Each one of these calls results in at least one network call.
-                create_ticket(conn, ticket_id, logical_path);
+                create_ticket(conn, ticket_id, _type, logical_path.c_str());
                 set_ticket_use_count(conn, ticket_id, _use_count);
+                set_ticket_write_file_count(conn, ticket_id, _write_file_count);
+                set_ticket_write_byte_count(conn, ticket_id, _write_byte_count);
                 set_ticket_expiration_timestamp(conn, ticket_id, _seconds_until_expiration);
+                set_ticket_allowed_users(conn, ticket_id, _users);
+                set_ticket_allowed_groups(conn, ticket_id, _groups);
+                set_ticket_allowed_hosts(conn, ticket_id, _hosts);
 
                 using json = nlohmann::json;
 
@@ -68,21 +81,24 @@ namespace irods::rest
                     {"headers", json::object({
                         {"irods-ticket", json::array({ticket_id})}
                     })},
-                    {"url", fmt::format("{}/stream?path={}&offset=0&count={}", base_url, logical_path, size)}
+                    {"url", fmt::format("{}/stream?path={}&offset=0&count={}", base_url, logical_path.c_str(), size)}
                 });
 
                 return std::make_tuple(Pistache::Http::Code::Ok, results.dump());
             }
             catch (const fs::filesystem_error& e) {
                 error("Caught exception - [error_code={}] {}", e.code().value(), e.what());
+                delete_ticket(_request, ticket_id);
                 return make_error_response(e.code().value(), e.what());
             }
             catch (const irods::exception& e) {
                 error("Caught exception - [error_code={}] {}", e.code(), e.what());
+                delete_ticket(_request, ticket_id);
                 return make_error_response(e.code(), e.what());
             }
             catch (const std::exception& e) {
                 error("Caught exception - {}", e.what());
+                delete_ticket(_request, ticket_id);
                 return make_error_response(SYS_INTERNAL_ERR, e.what());
             }
         } // operator()
@@ -121,12 +137,28 @@ namespace irods::rest
             return new_ticket;
         } // make_ticket_id
 
+        void delete_ticket(const Pistache::Rest::Request& _request, const std::string_view _ticket_id) noexcept
+        {
+            try {
+                auto conn = get_connection(_request.headers().getRaw("authorization").value());
+
+                const auto gql = fmt::format("select TICKET_ID where ticket_string = '{}'", _ticket_id);
+
+                if (irods::query q{conn(), gql}; q.size() == 0) {
+                    return;
+                }
+
+                rx_ticket(*conn(), "create", _ticket_id);
+            } catch (...) {}
+        } // delete_ticket
+
         void create_ticket(connection_proxy& _conn,
                            const std::string_view _ticket_id,
+                           const Pistache::Optional<std::string>& _ticket_type,
                            const std::string_view _logical_path) const
         {
             trace("Creating ticket [{}] ...", _ticket_id);
-            rx_ticket(*_conn(), "create", _ticket_id, "read", _logical_path);
+            rx_ticket(*_conn(), "create", _ticket_id, _ticket_type.getOrElse("read"), _logical_path);
         } // create_ticket
 
         void set_ticket_use_count(connection_proxy& _conn,
@@ -135,41 +167,138 @@ namespace irods::rest
         {
             trace("Setting use-count on ticket [{}] ...", _ticket_id);
 
-            const auto use_count = _use_count.getOrElse("0");
-            debug("use_count = [{}]", use_count);
-            rx_ticket(*_conn(), "mod", _ticket_id, "uses", use_count);
+            const auto count = _use_count.getOrElse("0");
+            debug("use_count = [{}]", count);
+            rx_ticket(*_conn(), "mod", _ticket_id, "uses", count);
         } // set_ticket_use_count
+
+        void set_ticket_write_file_count(connection_proxy& _conn,
+                                         const std::string_view _ticket_id,
+                                         const Pistache::Optional<std::string>& _file_count)
+        {
+            trace("Setting write-file count on ticket [{}] ...", _ticket_id);
+
+            const auto count = _file_count.getOrElse("0");
+            debug("write_file_count = [{}]", count);
+            rx_ticket(*_conn(), "mod", _ticket_id, "write-file", count);
+        } // set_ticket_write_file_count
+
+        void set_ticket_write_byte_count(connection_proxy& _conn,
+                                         const std::string_view _ticket_id,
+                                         const Pistache::Optional<std::string>& _byte_count)
+        {
+            trace("Setting write-byte count on ticket [{}] ...", _ticket_id);
+
+            const auto count = _byte_count.getOrElse("0");
+            debug("write_byte_count = [{}]", count);
+            rx_ticket(*_conn(), "mod", _ticket_id, "write-bytes", count);
+        } // set_ticket_write_byte_count
 
         void set_ticket_expiration_timestamp(connection_proxy& _conn,
                                              const std::string_view& _ticket_id,
                                              const Pistache::Optional<std::string>& _seconds_until_expiration) const
         {
+            if (_seconds_until_expiration.isEmpty()) {
+                return;
+            }
+
+            const auto secs_until_exp_string = _seconds_until_expiration.getOrElse("0");
+            debug("seconds until expiration = [{}]", secs_until_exp_string);
+
+            if ("0" == secs_until_exp_string) {
+                return;
+            }
+
             trace("Setting expiration on ticket [{}] ...", _ticket_id);
 
+            const auto secs = std::stoll(secs_until_exp_string);
+
+            if (secs < 0) {
+                const std::string_view msg_fmt = "seconds_until_expiration [{}] must be greater than or equal to zero.";
+                THROW(SYS_INVALID_INPUT_PARAM, fmt::format(msg_fmt, secs));
+            }
+
             using std::chrono::seconds;
-            using std::chrono::duration_cast;
+            using std::chrono::system_clock;
 
-            const auto secs_until_expiration = seconds(std::stoi(_seconds_until_expiration.getOrElse("30")));
-            const auto tp = std::chrono::system_clock::now() + secs_until_expiration;
-            debug("seconds_until_expiration = [{}]", secs_until_expiration.count());
+            const auto expiration_timestamp = system_clock::now() + seconds{secs};
+            const auto seconds_since_epoch = system_clock::to_time_t(expiration_timestamp);
 
-            const auto end_time = fmt::format("{}", duration_cast<seconds>(tp.time_since_epoch()).count());
-            rx_ticket(*_conn(), "mod", _ticket_id, "expire", end_time);
+            rx_ticket(*_conn(), "mod", _ticket_id, "expire", std::to_string(seconds_since_epoch));
         } // set_ticket_expiration_timestamp
 
+        void set_ticket_allowed_users(connection_proxy& _conn,
+                                      const std::string_view& _ticket_id,
+                                      const Pistache::Optional<std::string>& _users) const
+        {
+            if (_users.isEmpty()) {
+                return;
+            }
+
+            trace("Adding allowed users to ticket [{}] ...", _ticket_id);
+
+            std::vector<std::string> list;
+            boost::algorithm::split(list, _users.get(), boost::is_any_of(","));
+
+            for (auto&& user : list) {
+                debug("allowed user = [{}]", user);
+                rx_ticket(*_conn(), "mod", _ticket_id, "add", "user", user.data());
+            }
+        } // set_ticket_allowed_users
+
+        void set_ticket_allowed_groups(connection_proxy& _conn,
+                                       const std::string_view& _ticket_id,
+                                       const Pistache::Optional<std::string>& _groups) const
+        {
+            if (_groups.isEmpty()) {
+                return;
+            }
+
+            trace("Adding allowed groups to ticket [{}] ...", _ticket_id);
+
+            std::vector<std::string> list;
+            boost::algorithm::split(list, _groups.get(), boost::is_any_of(","));
+
+            for (auto&& group : list) {
+                debug("allowed group = [{}]", group);
+                rx_ticket(*_conn(), "mod", _ticket_id, "add", "group", group.data());
+            }
+        } // set_ticket_allowed_groups
+
+        void set_ticket_allowed_hosts(connection_proxy& _conn,
+                                      const std::string_view& _ticket_id,
+                                      const Pistache::Optional<std::string>& _hosts) const
+        {
+            if (_hosts.isEmpty()) {
+                return;
+            }
+
+            trace("Adding allowed hosts to ticket [{}] ...", _ticket_id);
+
+            std::vector<std::string> list;
+            boost::algorithm::split(list, _hosts.get(), boost::is_any_of(","));
+
+            for (auto&& host : list) {
+                debug("allowed host = [{}]", host);
+                rx_ticket(*_conn(), "mod", _ticket_id, "add", "host", host.data());
+            }
+        } // set_ticket_allowed_hosts
+
         void rx_ticket(RcComm& _conn,
-                       std::string_view _operation,
-                       std::string_view _ticket_id,
-                       std::string_view _type,
-                       std::string_view _value) const
+                       const std::string_view _operation,
+                       const std::string_view _ticket_id,
+                       const std::string_view _arg0 = "",
+                       const std::string_view _arg1 = "",
+                       const std::string_view _arg2 = "") const
         {
             trace("Invoking rcTicketAdmin() for ticket [{}] ...", _ticket_id);
 
             ticketAdminInp_t ticket_inp{};
             ticket_inp.arg1 = const_cast<char*>(_operation.data());
             ticket_inp.arg2 = const_cast<char*>(_ticket_id.data());
-            ticket_inp.arg3 = const_cast<char*>(_type.data());
-            ticket_inp.arg4 = const_cast<char*>(_value.data());
+            ticket_inp.arg3 = const_cast<char*>(_arg0.data());
+            ticket_inp.arg4 = const_cast<char*>(_arg1.data());
+            ticket_inp.arg5 = const_cast<char*>(_arg2.data());
 
             if (const auto ec = rcTicketAdmin(&_conn, &ticket_inp); ec < 0) {
                 THROW(ec, fmt::format("Received error from rcTicketAdmin for ticket [{}]", _ticket_id));
