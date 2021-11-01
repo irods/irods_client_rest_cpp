0feafb4cbb38ecdfaf91c8831b8d88469904db8d
Kory Draughn <korydraughn@renci.org>
2021-11-01 16:57:10 -0400

    [#85] Bumped version to 0.9.0.

--- jenkins_testing/ub_config_and_test_rest_clients.sh
+++ jenkins_testing/ub_config_and_test_rest_clients.sh
@@ -10,7 +10,7 @@ dir=$(dirname "$0")
 "$dir"/configure_and_start_iRODS.sh
 "$dir"/configure_and_start_NGINX.sh
 
-# curl -X POST -H "Authorization: Basic $(echo -n rods:rods|base64)" http://localhost:80/irods-rest/0.8.0/auth -o /tmp/output 
+# curl -X POST -H "Authorization: Basic $(echo -n rods:rods|base64)" http://localhost:80/irods-rest/0.9.0/auth -o /tmp/output 
 # sleep 300d
 
 su - irods -c "cd ~/scripts ; python run_tests.py --run_specific_test test_irods_client_rest_cpp"
