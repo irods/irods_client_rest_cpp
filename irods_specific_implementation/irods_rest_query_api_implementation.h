723bb069d63eae1cdf6b575f9f44f2eee88531e2
Kory Draughn <korydraughn@renci.org>
2021-11-02 14:11:55 -0400

    [#71] Added case-sensitive and distinct options to /query endpoint.

--- irods_specific_implementation/irods_rest_query_api_implementation.h
+++ irods_specific_implementation/irods_rest_query_api_implementation.h
@@ -5,6 +5,8 @@
 
 #include "constants.hpp"
 #include "irods_query.hpp"
+#include "rodsGenQuery.h"
+#include "rodsErrorTable.h"
 
 #include "pistache/router.h"
 
@@ -31,6 +33,8 @@ namespace irods::rest
                 auto _query_limit = _request.query().get("query_limit").get();
                 auto _row_offset = _request.query().get("row_offset").get();
                 auto _query_type = _request.query().get("query_type").get();
+                auto _case_sensitive = _request.query().get("case_sensitive").getOrElse("1");
+                auto _distinct = _request.query().get("distinct").getOrElse("1");
 
                 auto conn = get_connection(_request.headers().getRaw("authorization").value(), _query_string);
 
@@ -39,13 +43,15 @@ namespace irods::rest
                 uintmax_t row_offset  = std::stoi(_row_offset);
                 uintmax_t query_limit = std::stoi(_query_limit);
 
-                auto query_type = irods::query<rcComm_t>::convert_string_to_query_type(_query_type);
-                irods::query<rcComm_t> qobj{conn(), query_string, query_limit, row_offset, query_type};
+                const auto query_type = irods::query<rcComm_t>::convert_string_to_query_type(_query_type);
+                const auto options = init_query_options(_case_sensitive, _distinct);
+
+                irods::query query{conn(), query_string, query_limit, row_offset, query_type, options};
 
                 uintmax_t current_row_count = 0;
-                uintmax_t total_row_count = qobj.size();
+                uintmax_t total_row_count = query.size();
                 nlohmann::json arrays = nlohmann::json::array();
-                for (auto&& row : qobj) {
+                for (auto&& row : query) {
                     ++current_row_count;
 
                     nlohmann::json array = nlohmann::json::array();
@@ -67,39 +73,57 @@ namespace irods::rest
                 double dbl_query_limit = static_cast<double>(query_limit);
                 double dbl_total_row_count = static_cast<double>(total_row_count);
                 nlohmann::json links = nlohmann::json::object();
-                std::string url{base_url + "query?query_string={}&query_limit={}&row_offset={}&query_type={}"};
-                links["self"] = fmt::format(url, query_string, _query_limit, _row_offset, _query_type);
+                auto url = base_url + "query?query_string={}&query_limit={}&row_offset={}&query_type={}&case_sensitive={}&distinct={}";
+                links["self"] = fmt::format(url,
+                                            query_string,
+                                            _query_limit,
+                                            _row_offset,
+                                            _query_type,
+                                            _case_sensitive,
+                                            _distinct);
 
-                links["first"] = fmt::format(url, query_string, _query_limit, "0", _query_type);
+                links["first"] = fmt::format(url,
+                                             query_string,
+                                             _query_limit,
+                                             "0",
+                                             _query_type,
+                                             _case_sensitive,
+                                             _distinct);
 
                 double total_pages = dbl_total_row_count / dbl_query_limit;
-                double fraction_remaining_pages = (total_pages - std::trunc(total_pages));
+                double fraction_remaining_pages = total_pages - std::trunc(total_pages);
                 double remaining_rows = fraction_remaining_pages * static_cast<double>(dbl_query_limit);
-                double final_page_delta{remaining_rows == 0.0 ? dbl_query_limit : remaining_rows};
-                double last_page_number{dbl_total_row_count - final_page_delta};
+                double final_page_delta = (remaining_rows == 0.0) ? dbl_query_limit : remaining_rows;
+                double last_page_number = dbl_total_row_count - final_page_delta;
 
-                links["last"] = fmt::format(url
-                                , query_string
-                                , _query_limit
-                                , std::to_string(static_cast<int>(last_page_number))
-                                , _query_type);
+                links["last"] = fmt::format(url,
+                                            query_string,
+                                            _query_limit,
+                                            static_cast<int>(last_page_number),
+                                            _query_type,
+                                            _case_sensitive,
+                                            _distinct);
 
                 double current_page_number = dbl_row_offset / dbl_query_limit;
-                double next_page_number{std::trunc(current_page_number) + 1 * dbl_query_limit};
+                double next_page_number = std::trunc(current_page_number) + 1 * dbl_query_limit;
                 next_page_number = (next_page_number >= dbl_total_row_count) ? last_page_number : next_page_number;
 
-                links["next"] = fmt::format(url
-                                , query_string
-                                , _query_limit
-                                , std::to_string(static_cast<int>(next_page_number))
-                                , _query_type);
+                links["next"] = fmt::format(url,
+                                            query_string,
+                                            _query_limit,
+                                            static_cast<int>(next_page_number),
+                                            _query_type,
+                                            _case_sensitive,
+                                            _distinct);
 
                 auto prev_count = dbl_row_offset - dbl_query_limit;
-                links["prev"] = fmt::format(url
-                                , query_string
-                                , _query_limit
-                                , std::to_string(static_cast<int>(std::max(0.0, prev_count)))
-                                , _query_type);
+                links["prev"] = fmt::format(url,
+                                            query_string,
+                                            _query_limit,
+                                            static_cast<int>(std::max(0.0, prev_count)),
+                                            _query_type,
+                                            _case_sensitive,
+                                            _distinct);
                 results["_links"] = links;
 
                 return std::make_tuple(Pistache::Http::Code::Ok, results.dump());
@@ -113,6 +137,29 @@ namespace irods::rest
                 return make_error_response(SYS_INVALID_INPUT_PARAM, e.what());
             }
         } // operator()
+
+    private:
+        int init_query_options(const std::string_view _case_sensitive,
+                               const std::string_view _distinct)
+        {
+            int options = 0;
+
+            if (_case_sensitive == "0") {
+                options |= UPPER_CASE_WHERE;
+            }
+            else if (_case_sensitive != "1") {
+                THROW(SYS_INVALID_INPUT_PARAM, "Invalid query option: case_sensitive must be 0 or 1.");
+            }
+
+            if (_distinct == "0") {
+                options |= NO_DISTINCT;
+            }
+            else if (_distinct != "1") {
+                THROW(SYS_INVALID_INPUT_PARAM, "Invalid query option: distinct must be 0 or 1.");
+            }
+
+            return options;
+        } // init_query_options
     }; // class query
 } // namespace irods::rest
 
